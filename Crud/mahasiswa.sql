7b520cd3ce6ab96ce6c37cb3256c0985855adabb
andrianto2002 <117975401+andrianto2002@users.noreply.github.com>
2023-01-08 21:16:46 +0700

    Add files via upload

---
+++ Crud/mahasiswa.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 08, 2023 at 03:11 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kry`
+--
+
+CREATE TABLE `kry` (
+  `ID` int(50) NOT NULL,
+  `NAMA` varchar(200) NOT NULL,
+  `JABATAN` varchar(225) NOT NULL,
+  `ALAMAT` varchar(225) NOT NULL,
+  `NOTELPON` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kry`
+--
+
+INSERT INTO `kry` (`ID`, `NAMA`, `JABATAN`, `ALAMAT`, `NOTELPON`) VALUES
+(9, 'p', 'DIREKTUR', 'p', 9);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kry`
+--
+ALTER TABLE `kry`
+  ADD PRIMARY KEY (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
